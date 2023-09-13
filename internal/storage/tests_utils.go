4dac76e0db1d4929ebd11c01d0da945f30b40fc3
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2023-09-13 15:02:18 +0200

    Update tests_utils.go

--- internal/storage/tests_utils.go
+++ internal/storage/tests_utils.go
@@ -117,7 +117,7 @@ func InitTestDBTables(t *testing.T, connectionURL string) (func(), error) {
 		return cleanupFunc, nil
 	}
 
-	dirPath := "./migrations/"
+	dirPath := "./../../migrations"
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		log.Printf("Cannot read files from directory %s", dirPath)
