dabbd4c5007d94e4749198a1fe86dfbd5047da7f
Marcin Szwed <marcin.szwed@sap.com>
2023-10-16 14:38:56 +0200

    Fix Kyma Environments Cleanup CronJob (#107)
    Update cronjob manifest and script

--- utils/kyma-environments-cleanup-job/apply.sh
+++ utils/kyma-environments-cleanup-job/apply.sh
@@ -12,12 +12,9 @@ kebContainerName=kyma-environment-broker
 cloudsqlProxyContainerName=cloudsql-proxy
 host=kyma-env-broker
 
-SCRIPT_DOMAIN=${SCRIPT_BROKER_URL#"$host."}
-
 SCRIPT_CLOUDSQL_PROXY_COMMAND=$(kubectl get deployment $deploymentName -n $namespace -o jsonpath=\
 "{.spec.template.spec.containers[?(@.name==\"$cloudsqlProxyContainerName\")].command}")
 
-export SCRIPT_DOMAIN
 export SCRIPT_CLOUDSQL_PROXY_COMMAND
 
 envsubst < kyma-environments-cleanup-job.yaml | kubectl apply -f -
