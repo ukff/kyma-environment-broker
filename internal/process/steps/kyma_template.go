98e0bd616c7f02beefbbcfe426099537414fbc30
ukff <110393214+ukff@users.noreply.github.com>
2023-10-24 14:43:55 +0200

    wip - temp commit msg

--- internal/process/steps/kyma_template.go
+++ internal/process/steps/kyma_template.go
@@ -2,20 +2,16 @@ package steps
 
 import (
 	"bytes"
-	"fmt"
 	"time"
-
-	"gopkg.in/yaml.v2"
-
-	"github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	k8syaml "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	k8syamlutil "k8s.io/apimachinery/pkg/util/yaml"
 
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/process"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 )
 
 type InitKymaTemplate struct {
@@ -33,22 +29,16 @@ func (s *InitKymaTemplate) Name() string {
 }
 
 func (s *InitKymaTemplate) Run(operation internal.Operation, logger logrus.FieldLogger) (internal.Operation, time.Duration, error) {
-	kymaTemplate := operation.InputCreator.Configuration().KymaTemplate
-	decodeKymaTemplate, err := DecodeKymaTemplate(kymaTemplate)
+	tmpl := operation.InputCreator.Configuration().KymaTemplate
+	obj, err := DecodeKymaTemplate(tmpl)
 	if err != nil {
 		logger.Errorf("Unable to create kyma template: %s", err.Error())
 		return s.operationManager.OperationFailed(operation, "unable to create a kyma template", err, logger)
 	}
-	if decodeKymaTemplate == nil {
-		logger.Error("Unable to create kyma template, object is empty")
-		return s.operationManager.OperationFailed(operation, "Unable to create kyma template, object is empty", err, logger)
-	}
-	logger.Infof("Decoded kyma template: %v", decodeKymaTemplate)
-
-	logger.Info("applied kyma will be: %s", kymaTemplate)
+	logger.Infof("Decoded kyma template: %v", obj)
 	return s.operationManager.UpdateOperation(operation, func(op *internal.Operation) {
-		op.KymaResourceNamespace = decodeKymaTemplate.GetNamespace()
-		op.KymaTemplate = kymaTemplate
+		op.KymaResourceNamespace = obj.GetNamespace()
+		op.KymaTemplate = tmpl
 	}, logger)
 }
 
@@ -68,24 +58,18 @@ func (s initKymaTemplateUpgradeKyma) Run(o internal.UpgradeKymaOperation, logger
 
 func DecodeKymaTemplate(template string) (*unstructured.Unstructured, error) {
 	tmpl := []byte(template)
-	decoder := k8syamlutil.NewYAMLOrJSONDecoder(bytes.NewReader(tmpl), 512)
+
+	decoder := yamlutil.NewYAMLOrJSONDecoder(bytes.NewReader(tmpl), 512)
 	var rawObj runtime.RawExtension
 	if err := decoder.Decode(&rawObj); err != nil {
 		return nil, err
 	}
-	obj, _, err := k8syaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme).Decode(rawObj.Raw, nil, nil)
+	obj, _, err := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme).Decode(rawObj.Raw, nil, nil)
 	if err != nil {
 		return nil, err
 	}
+
 	unstructuredMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
 	unstructuredObj := &unstructured.Unstructured{Object: unstructuredMap}
 	return unstructuredObj, err
-}
-
-func encodeKymaTemplate(tmpl *unstructured.Unstructured) (string, error) {
-	result, err := yaml.Marshal(tmpl.Object)
-	if err != nil {
-		return "", fmt.Errorf("while marshal unstructured to yaml: %v", err)
-	}
-	return string(result), nil
 }
