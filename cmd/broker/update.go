dbbbf143f11fa192a9131f17ec3f59cb8c3d3edf
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2023-10-25 16:07:31 +0200

    Update operation docs (#95)
    * Update operation docs
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Update docs/03-03-runtime-operations.md
    
    Co-authored-by: Jarosław Pieszka <jaroslaw.pieszka@sap.com>
    
    * fix buiding
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Apply suggestions from code review
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * wip
    
    ---------
    
    Co-authored-by: Jarosław Pieszka <jaroslaw.pieszka@sap.com>
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>

---
+++ cmd/broker/update.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/kyma-project/kyma-environment-broker/internal"
+	"github.com/kyma-project/kyma-environment-broker/internal/event"
+	"github.com/kyma-project/kyma-environment-broker/internal/process"
+	"github.com/kyma-project/kyma-environment-broker/internal/process/input"
+	"github.com/kyma-project/kyma-environment-broker/internal/process/update"
+	"github.com/kyma-project/kyma-environment-broker/internal/provisioner"
+	"github.com/kyma-project/kyma-environment-broker/internal/reconciler"
+	"github.com/kyma-project/kyma-environment-broker/internal/runtimeversion"
+	"github.com/kyma-project/kyma-environment-broker/internal/storage"
+	"github.com/sirupsen/logrus"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func NewUpdateProcessingQueue(ctx context.Context, manager *process.StagedManager, workersAmount int, db storage.BrokerStorage, inputFactory input.CreatorForPlan,
+	provisionerClient provisioner.Client, publisher event.Publisher, runtimeVerConfigurator *runtimeversion.RuntimeVersionConfigurator, runtimeStatesDb storage.RuntimeStates,
+	runtimeProvider input.ComponentListProvider, reconcilerClient reconciler.Client, cfg Config, k8sClientProvider func(kcfg string) (client.Client, error), cli client.Client, logs logrus.FieldLogger) *process.Queue {
+
+	requiresReconcilerUpdate := update.RequiresReconcilerUpdate
+	if cfg.ReconcilerIntegrationDisabled {
+		requiresReconcilerUpdate = func(op internal.Operation) bool { return false }
+	}
+	manager.DefineStages([]string{"cluster", "btp-operator", "btp-operator-check", "check"})
+	updateSteps := []struct {
+		stage     string
+		step      process.Step
+		condition process.StepCondition
+	}{
+		{
+			stage: "cluster",
+			step:  update.NewInitialisationStep(db.Instances(), db.Operations(), runtimeVerConfigurator, inputFactory),
+		},
+		{
+			stage:     "cluster",
+			step:      update.NewUpgradeShootStep(db.Operations(), db.RuntimeStates(), provisionerClient),
+			condition: update.SkipForOwnClusterPlan,
+		},
+		{
+			stage: "btp-operator",
+			step:  update.NewInitKymaVersionStep(db.Operations(), runtimeVerConfigurator, runtimeStatesDb),
+		},
+		{
+			stage:     "btp-operator",
+			step:      update.NewGetKubeconfigStep(db.Operations(), provisionerClient, k8sClientProvider),
+			condition: update.ForBTPOperatorCredentialsProvided,
+		},
+		{
+			stage:     "btp-operator",
+			step:      update.NewBTPOperatorOverridesStep(db.Operations(), runtimeProvider),
+			condition: update.RequiresBTPOperatorCredentials,
+		},
+		{
+			stage:     "btp-operator",
+			step:      update.NewApplyReconcilerConfigurationStep(db.Operations(), db.RuntimeStates(), reconcilerClient),
+			condition: requiresReconcilerUpdate,
+		},
+		{
+			stage:     "btp-operator-check",
+			step:      update.NewCheckReconcilerState(db.Operations(), reconcilerClient),
+			condition: update.CheckReconcilerStatus,
+		},
+		{
+			stage:     "check",
+			step:      update.NewCheckStep(db.Operations(), provisionerClient, 40*time.Minute),
+			condition: update.SkipForOwnClusterPlan,
+		},
+	}
+
+	for _, step := range updateSteps {
+		err := manager.AddStep(step.stage, step.step, step.condition)
+		if err != nil {
+			fatalOnError(err)
+		}
+	}
+	queue := process.NewQueue(manager, logs)
+	queue.Run(ctx.Done(), workersAmount)
+
+	return queue
+}
---
+++ cmd/broker/upgrade_cluster.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	orchestrationExt "github.com/kyma-project/kyma-environment-broker/common/orchestration"
+	"github.com/kyma-project/kyma-environment-broker/internal/avs"
+	"github.com/kyma-project/kyma-environment-broker/internal/event"
+	"github.com/kyma-project/kyma-environment-broker/internal/notification"
+	"github.com/kyma-project/kyma-environment-broker/internal/orchestration/manager"
+	"github.com/kyma-project/kyma-environment-broker/internal/process"
+	"github.com/kyma-project/kyma-environment-broker/internal/process/input"
+	"github.com/kyma-project/kyma-environment-broker/internal/process/provisioning"
+	"github.com/kyma-project/kyma-environment-broker/internal/process/upgrade_cluster"
+	"github.com/kyma-project/kyma-environment-broker/internal/provisioner"
+	"github.com/kyma-project/kyma-environment-broker/internal/storage"
+	"github.com/sirupsen/logrus"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func NewClusterOrchestrationProcessingQueue(ctx context.Context, db storage.BrokerStorage, provisionerClient provisioner.Client,
+	pub event.Publisher, inputFactory input.CreatorForPlan, icfg *upgrade_cluster.TimeSchedule, pollingInterval time.Duration,
+	runtimeResolver orchestrationExt.RuntimeResolver, upgradeEvalManager *avs.EvaluationManager, notificationBuilder notification.BundleBuilder, logs logrus.FieldLogger,
+	cli client.Client, cfg Config, speedFactor int) *process.Queue {
+
+	upgradeClusterManager := upgrade_cluster.NewManager(db.Operations(), pub, logs.WithField("upgradeCluster", "manager"))
+	upgradeClusterInit := upgrade_cluster.NewInitialisationStep(db.Operations(), db.Orchestrations(), provisionerClient, inputFactory, upgradeEvalManager, icfg, notificationBuilder)
+	upgradeClusterManager.InitStep(upgradeClusterInit)
+
+	upgradeClusterSteps := []struct {
+		disabled  bool
+		weight    int
+		step      upgrade_cluster.Step
+		condition upgrade_cluster.StepCondition
+	}{
+		{
+			weight:    1,
+			step:      upgrade_cluster.NewLogSkippingUpgradeStep(db.Operations()),
+			condition: provisioning.DoForOwnClusterPlanOnly,
+		},
+		{
+			weight:    10,
+			step:      upgrade_cluster.NewSendNotificationStep(db.Operations(), notificationBuilder),
+			condition: provisioning.SkipForOwnClusterPlan,
+		},
+		{
+			weight:    10,
+			step:      upgrade_cluster.NewUpgradeClusterStep(db.Operations(), db.RuntimeStates(), provisionerClient, icfg),
+			condition: provisioning.SkipForOwnClusterPlan,
+		},
+	}
+
+	for _, step := range upgradeClusterSteps {
+		if !step.disabled {
+			upgradeClusterManager.AddStep(step.weight, step.step, step.condition)
+		}
+	}
+
+	orchestrateClusterManager := manager.NewUpgradeClusterManager(db.Orchestrations(), db.Operations(), db.Instances(),
+		upgradeClusterManager, runtimeResolver, pollingInterval, logs.WithField("upgradeCluster", "orchestration"),
+		cli, cfg.OrchestrationConfig, notificationBuilder, speedFactor)
+	queue := process.NewQueue(orchestrateClusterManager, logs)
+
+	queue.Run(ctx.Done(), 3)
+
+	return queue
+}
