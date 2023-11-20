4c64ca232e966029605291dfc4f52abf245ae02f
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-11-20 16:03:49 +0100

    Create or update sec. scanning config file in the release workflow (#176)
    * Sec. scanning config file in the release workflow
    
    * wip
    
    * wip
    
    * Add keb repo env
    
    * Update sec scanner config script
    
    * Scanner args mandatory
    
    * Check for all KEB related images
    
    * Refactor check for artifacts
    
    * Update bot name
    
    ---------
    
    Co-authored-by: Miskiewicz <piotr.miskiewicz@sap.com>

--- scripts/check_artifacts_existence.sh
+++ scripts/check_artifacts_existence.sh
@@ -9,19 +9,25 @@ set -o pipefail # prevents errors in a pipeline from being masked
 # This script has the following arguments:
 #                       -  image tag - mandatory
 #
-# ./check_artifacts_existence.sh v2.1.0
+# ./check_artifacts_existence.sh 2.1.0
 
-# Expected variables:
-#             IMAGE_REPO - binary image repository
-#             GITHUB_TOKEN - github token
 
 export IMAGE_TAG=$1
 
 PROTOCOL=docker://
+IMAGE_NAMES=(
+  kyma-environment-broker
+  kyma-environment-deprovision-retrigger-job
+  kyma-environment-runtime-reconciler
+  kyma-environment-subaccount-cleanup-job
+  kyma-environment-trial-cleanup-job
+  kyma-environments-cleanup-job
+)
 
-if [ $(skopeo list-tags ${PROTOCOL}${IMAGE_REPO} | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment Broker binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment Broker binary image found for tag ${IMAGE_TAG}"
-fi
+for image in "${IMAGE_NAMES[@]}"; do
+  if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/${image} | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]; then
+    echo "::warning ::${image} binary image for tag ${IMAGE_TAG} already exists"
+  else
+    echo "No previous ${image} binary image found for tag ${IMAGE_TAG}"
+  fi
+done
