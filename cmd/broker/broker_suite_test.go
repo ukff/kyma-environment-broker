467808d7b5c6ed267f0597b0e8f21677e4b65f84
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-11-28 16:44:18 +0100

    Increase poller timeout in unit tests (#220)
    Increase poller timeout

--- cmd/broker/broker_suite_test.go
+++ cmd/broker/broker_suite_test.go
@@ -240,7 +240,7 @@ func NewBrokerSuiteTest(t *testing.T, version ...string) *BrokerSuiteTest {
 		k8sKcp:              cli,
 		k8sSKR:              fakeK8sSKRClient,
 	}
-	ts.poller = &broker.TimerPoller{PollInterval: 3 * time.Millisecond, PollTimeout: 5 * time.Second, Log: ts.t.Log}
+	ts.poller = &broker.TimerPoller{PollInterval: 3 * time.Millisecond, PollTimeout: 30 * time.Second, Log: ts.t.Log}
 
 	ts.CreateAPI(inputFactory, cfg, db, provisioningQueue, deprovisioningQueue, updateQueue, logs)
 
