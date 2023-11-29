753f338411612f89071c37c54dfdbc35b5f86b61
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-11-29 13:48:18 +0100

    Add keb helm chart tests (#226)
    * Add helm template workflow
    
    * Fix path
    
    * Apply helm chart
    
    * Add registries config
    
    * Fix chart directory
    
    * Remove istio
    
    * Revert "Remove istio"
    
    This reverts commit 24a39cb7b27532ef935d4b5696fc8220aac6faeb.
    
    * Break helm chart
    
    * Change step order
    
    * Fix helm chart
    
    * Check status
    
    * Squashed commit of the following:
    
    commit 4b6d1baa7402f0598d2afaed043ccd1d9e265c86
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 12:13:14 2023 +0100
    
        Testing
    
    commit 3227c32d59de3cf6c2163c78d4733ea57f26e4a5
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 12:12:09 2023 +0100
    
        Testing
    
    commit 89e8bbb35a0e2e2870b5ea98e5a5aa17272237e4
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 12:05:50 2023 +0100
    
        Testing
    
    commit a975a3fed4a86d2792cba3a46ad3b599f1c848f8
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 12:02:59 2023 +0100
    
        Testing
    
    commit 64ec208f957f61fd6deaffcc910d69e9753522ae
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 12:01:30 2023 +0100
    
        Testing
    
    commit 19ea359568f974515526b0746a9eda02eadf8729
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 11:54:41 2023 +0100
    
        Testing
    
    commit 3e136ee12b36b0dc8b15f151e22cd2a5d20eca3a
    Author: Marek Michali <marek.michali@sap.com>
    Date:   Wed Nov 29 11:49:58 2023 +0100
    
        Testing
    
    * Change output type
    
    * Refactor

---
+++ scripts/testing/get-latest-k3s-releases.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+LIST_LEN=${1:-5}
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+
+REPOSITORY=k3s-io/k3s
+GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
+
+# selecting at most ${LIST_LEN} recent minor version releases with maximal patch number for the given minor
+# Example
+#    given:
+#             "v1.24.17+k3s1"
+#             "v1.25.13+k3s1"
+#             "v1.25.14+k3s1"
+#             "v1.26.8+k3s1"
+#             "v1.26.9+k3s1"
+#             "v1.27.5+k3s1"
+#             "v1.27.6+k3s1"
+#             "v1.28.1+k3s1"
+#             "v1.28.2+k3s1"
+#   outputs:
+#             "v1.25.14+k3s1" "v1.26.9+k3s1" "v1.27.6+k3s1" "v1.28.1+k3s1"
+LATEST_RELEASES=($(curl -sS "${GITHUB_URL}/releases" \
+| jq '.[] | select(.name|test("v[0-9]{1,2}.[0-9]{1,3}.[0-9]{1,3}\\+")) | .name' \
+| sort -rV\
+| awk -F. '/k3s1/ {if ($2 != p) {print $0; p=$2}}' \
+| head -n ${LIST_LEN}))
+
+echo ${LATEST_RELEASES[*]}
