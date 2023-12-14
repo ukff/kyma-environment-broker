47efd893a390243eed70a09fa1952b60e00c2d41
Marcin Szwed <marcin.szwed@sap.com>
2023-12-14 12:41:25 +0100

    Fix Trial Suspension E2E test false positive (#275)
    Use --bail flag to stop test execution after first fail

--- testing/e2e/skr/trial-suspension-test/test.js
+++ testing/e2e/skr/trial-suspension-test/test.js
@@ -27,50 +27,34 @@ describe('SKR Trial suspension test', function() {
   this.slow(slowTestDuration);
 
   let suspensionOpID;
-  let skipTests = false;
   const options = gatherOptions();
 
   before('Ensure SKR Trial is provisioned', async function() {
     try {
       await callFuncAndPrintExecutionTime(getOrProvisionSKR, [options, false, provisioningTimeout]);
     } catch (e) {
-      console.log(`${e.toString()}\n`);
-      skipTests = true;
+      throw new Error(`${e.toString()}\n`);
     }
   });
 
   it('should wait until Trial Cleanup CronJob triggers suspension', async function() {
-    if (skipTests) {
-      console.log(`skipping test due to failures`);
-      this.skip();
-    }
-
     try {
       const rs = await callFuncAndPrintExecutionTime(ensureSuspensionIsInProgress,
           [kcp, options.instanceID, trialCleanupTriggerTimeout]);
       suspensionOpID = rs.data[0].status[suspensionOperationType].data[0].operationID;
       assert.isDefined(suspensionOpID, `suspension operation ID: ${suspensionOpID}`);
     } catch (e) {
-      console.log(`${e.toString()}\n`);
-      skipTests = true;
-      this.skip();
+      throw new Error(`${e.toString()}\n`);
     }
   });
 
   it('should wait until suspension succeeds', async function() {
-    if (skipTests) {
-      console.log(`skipping test due to failures`);
-      this.skip();
-    }
-
     try {
       debug(`Waiting until suspension operation succeeds...`);
       await callFuncAndPrintExecutionTime(ensureSuspensionSucceeded,
           [keb, kcp, options.instanceID, suspensionOpID, suspensionTimeout]);
     } catch (e) {
-      console.log(`${e.toString()}\n`);
-      skipTests = true;
-      this.skip();
+      throw new Error(`${e.toString()}\n`);
     }
   });
 
