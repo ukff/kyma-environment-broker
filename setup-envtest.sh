7d405ed8606aa5f8d79167f6219c00253dca7c28
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2023-12-23 10:40:11 +0100

    Separate envtest bin directory for every test (#310)
    separate envtest bin directory for every test

--- setup-envtest.sh
+++ setup-envtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-LOCAL_BIN=$(pwd)/bin
+LOCAL_BIN=$(pwd)/$$/bin
 mkdir -p $LOCAL_BIN
 GOBIN=$LOCAL_BIN go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 OUTPUT=$($LOCAL_BIN/setup-envtest use --bin-dir $LOCAL_BIN -p path latest)
