76a36c49da73bdd709e129793bf4d5eee603878c
ukff <110393214+ukff@users.noreply.github.com>
2024-01-23 09:08:16 +0100

    wip - temp commit msg

--- internal/storage/driver/postsql/initialization_test.go
+++ internal/storage/driver/postsql/initialization_test.go
@@ -2,7 +2,7 @@ package postsql_test
 
 import (
 	"testing"
-
+	
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage/postsql"
 	"github.com/sirupsen/logrus"
@@ -15,26 +15,26 @@ const (
 )
 
 func TestInitialization(t *testing.T) {
-
+	
 	t.Skip()
 	t.Run("Should initialize database when schema not applied", func(t *testing.T) {
 		cleanup, cfg, err := storage.InitTestDB(t)
-		cleanup()
+		defer cleanup()
 		require.NoError(t, err)
-
+		
 		// when
 		connection, err := postsql.InitializeDatabase(cfg.ConnectionURL(), maxTestDbAccessRetries, logrus.New())
 		require.NoError(t, err)
 		require.NotNil(t, connection)
 	})
-
+	
 	t.Run("Should return error when failed to connect to the database with bad connection string", func(t *testing.T) {
 		// given
 		connString := "bad connection string"
-
+		
 		// when
 		connection, err := postsql.InitializeDatabase(connString, 3, logrus.New())
-
+		
 		// then
 		assert.Error(t, err)
 		assert.Nil(t, connection)
