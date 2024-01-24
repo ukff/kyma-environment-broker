cdd39c34a51d0e2f7724a68996e9082f3f24defb
ukff <110393214+ukff@users.noreply.github.com>
2024-01-24 21:40:16 +0100

    wip - temp commit msg

--- internal/storage/driver/postsql/postsql_test.go
+++ internal/storage/driver/postsql/postsql_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 	"time"
-
+	
 	"github.com/gocraft/dbr"
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/events"
@@ -44,9 +44,9 @@ func TestMain(m *testing.M) {
 	defer func() {
 		os.Exit(exitVal)
 	}()
-
+	
 	config := brokerStorageTestConfig()
-
+	
 	docker, err := internal.NewDockerHandler()
 	if err != nil {
 		log.Fatal(err)
@@ -57,7 +57,7 @@ func TestMain(m *testing.M) {
 			log.Fatal(err)
 		}
 	}(docker)
-
+	
 	cleanupContainer, err := docker.CreateDBContainer(internal.ContainerCreateRequest{
 		Port:          config.Port,
 		User:          config.User,
@@ -78,7 +78,7 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	exitVal = m.Run()
 }
 
@@ -94,16 +94,16 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 	if storage == nil {
 		return nil, nil, fmt.Errorf("storage is nil")
 	}
-
+	
 	failOnIncorrectDB(connection, config)
 	failOnNotEmptyDb(connection)
-
+	
 	err = runMigrations(connection, Up)
 	if err != nil {
 		return nil, nil, fmt.Errorf("while applying migration files: %w", err)
 	}
 	log.Println("db created")
-
+	
 	cleanup := func() error {
 		failOnIncorrectDB(connection, config)
 		fmt.Println("cleaning up")
@@ -114,7 +114,7 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 		fmt.Println("cleaned up")
 		return nil
 	}
-
+	
 	return cleanup, storage, nil
 }
 
@@ -122,23 +122,23 @@ func runMigrations(connection *dbr.Connection, order migrationOrder) error {
 	if order != Up && order != Down {
 		return fmt.Errorf("unknown migration order")
 	}
-
+	
 	migrations := "./../../../../resources/keb/migrations/"
 	files, err := os.ReadDir(migrations)
 	if err != nil {
 		return fmt.Errorf("while reading migration data: %w in directory :%s", err, migrations)
 	}
-
+	
 	suffix := ""
 	if order == Down {
 		suffix = "down.sql"
 		slices.Reverse(files)
 	}
-
+	
 	if order == Up {
 		suffix = "up.sql"
 	}
-
+	
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), suffix) {
 			content, err := os.ReadFile(migrations + file.Name())
@@ -150,7 +150,7 @@ func runMigrations(connection *dbr.Connection, order migrationOrder) error {
 			}
 		}
 	}
-
+	
 	return nil
 }
 
@@ -185,18 +185,18 @@ func failOnNotEmptyDb(db *dbr.Connection) {
 		fmt.Println(fmt.Sprintf("table lookup result: %s", rowResult))
 		return rowResult != "", nil
 	}
-
+	
 	exists, err := tableExists(postsql.OperationTableName)
 	if err != nil {
-		panic(fmt.Sprintf("cannot verify if table %s exists", postsql.OperationTableName))
+		panic(fmt.Sprintf("cannot verify if table %s exists: %s", postsql.OperationTableName, err.Error()))
 	}
 	if exists {
 		panic(fmt.Sprintf("in db, data for %s are in table", postsql.OperationTableName))
 	}
-
+	
 	exists, err = tableExists(postsql.InstancesTableName)
 	if err != nil {
-		panic(fmt.Sprintf("cannot verify if table %s exists", postsql.InstancesTableName))
+		panic(fmt.Sprintf("cannot verify if table %s exists: %s", postsql.InstancesTableName, err.Error()))
 	}
 	if exists {
 		panic(fmt.Sprintf("in db, data for %s are in table", postsql.InstancesTableName))
