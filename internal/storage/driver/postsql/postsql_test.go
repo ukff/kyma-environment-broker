e1e749e34c46038f85d987a1a8ef0db46ee71cb0
ukff <110393214+ukff@users.noreply.github.com>
2024-01-24 20:27:43 +0100

    wip - temp commit msg

--- internal/storage/driver/postsql/postsql_test.go
+++ internal/storage/driver/postsql/postsql_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 	"time"
-	
+
 	"github.com/gocraft/dbr"
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/events"
@@ -26,7 +26,7 @@ func brokerStorageTestConfig() storage.Config {
 		kebStorageTestDbPort    = "5430"
 		kebStorageTestSecretKey = "################################"
 	)
-	
+
 	return storage.Config{
 		Host:            kebStorageTestHostname,
 		User:            kebStorageTestDbUser,
@@ -46,16 +46,16 @@ func TestMain(m *testing.M) {
 	defer func() {
 		os.Exit(exitVal)
 	}()
-	
+
 	config := brokerStorageTestConfig()
 	fmt.Println(fmt.Sprintf("connection URL -> %s", config.ConnectionURL()))
-	
+
 	docker, err := internal.NewDockerHandler()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer docker.CloseDockerClient()
-	
+
 	cleanupContainer, err := docker.CreateDBContainer(internal.ContainerCreateRequest{
 		Port:          config.Port,
 		User:          config.User,
@@ -65,7 +65,7 @@ func TestMain(m *testing.M) {
 		ContainerName: "keb-storage-tests",
 		Image:         "postgres:11",
 	})
-	
+
 	log.Print("container started")
 	defer func() {
 		log.Println("cleaning up")
@@ -80,9 +80,9 @@ func TestMain(m *testing.M) {
 		log.Println("error while starting container")
 		log.Fatal(err)
 	}
-	
+
 	fmt.Println(fmt.Sprintf("connection URL -> %s", config.ConnectionURL()))
-	
+
 	exitVal = m.Run()
 }
 
@@ -102,13 +102,13 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 	fmt.Println(fmt.Sprintf("connected to URL -> %s", config.ConnectionURL()))
 	failOnIncorrectDB(connection, config)
 	// failOnNotEmptyDb(connection, storage)
-	
+
 	migrations := "./../../../../resources/keb/migrations/"
 	files, err := os.ReadDir(migrations)
 	if err != nil {
 		return nil, nil, fmt.Errorf("while reading migration data: %w in directory :%s", err, migrations)
 	}
-	
+
 	// tx, err := connection.BeginTx(context.Background(), nil)
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), "up.sql") {
@@ -121,7 +121,7 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 			}
 		}
 	}
-	
+
 	/*if commitErr := tx.Commit(); commitErr != nil {
 		err := tx.Rollback()
 		if err != nil {
@@ -130,7 +130,7 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 		return nil, nil, fmt.Errorf("while committing migration files: %w", commitErr)
 	}*/
 	fmt.Println("migration files applied")
-	
+
 	cleanup := func() error {
 		failOnIncorrectDB(connection, config)
 		fmt.Println("cleaning up")
@@ -146,7 +146,7 @@ func GetStorageForTests() (func() error, storage.BrokerStorage, error) {
 		fmt.Println("cleaned up")
 		return nil
 	}
-	
+
 	return cleanup, storage, nil
 }
 
@@ -180,7 +180,7 @@ func failOnNotEmptyDb(db *dbr.Connection, storage storage.BrokerStorage) {
 			result.Scan(rowResult)
 			return strings.EqualFold(rowResult, "1")
 		}*/
-	
+
 	_, len1, len2, err := storage.Instances().List(dbmodel.InstanceFilter{})
 	if err != nil {
 		panic(fmt.Sprintf("while checking len data for: %s , %s", postsql.InstancesTableName, err.Error()))
@@ -188,7 +188,7 @@ func failOnNotEmptyDb(db *dbr.Connection, storage storage.BrokerStorage) {
 	if len1 > 0 || len2 > 0 {
 		panic(fmt.Sprintf("storage for: %s is not empty", postsql.InstancesTableName))
 	}
-	
+
 	_, len1, len2, err = storage.Operations().ListOperations(dbmodel.OperationFilter{})
 	if err != nil {
 		panic(fmt.Sprintf("while checking len data for: %s , %s", postsql.OperationTableName, err.Error()))
@@ -196,12 +196,12 @@ func failOnNotEmptyDb(db *dbr.Connection, storage storage.BrokerStorage) {
 	if len1 > 0 || len2 > 0 {
 		panic(fmt.Sprintf("storage for: %s is not empty", postsql.OperationTableName))
 	}
-	
+
 	/*
 		if rowsExists(postsql.InstancesTableName) {
 			panic(fmt.Sprintf("in db, data for %s are in table", postsql.InstancesTableName))
 		}
-	
+
 		if rowsExists(postsql.InstancesTableName) {
 			panic(fmt.Sprintf("in db, data for %s are in table", postsql.InstancesTableName))
 		}*/
