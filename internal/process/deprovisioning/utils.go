629998b22b60f536d5379bd054ae49a6819313dc
ukff <110393214+ukff@users.noreply.github.com>
2024-03-11 12:26:41 +0100

    wip

--- internal/process/deprovisioning/utils.go
+++ internal/process/deprovisioning/utils.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	testInstanceID  = "58f8c703-1756-48ab-9299-a847974d1fee"
-	testOperationID  = "fd5cee4d-0eeb-40d0-a7a7-0708e5eba470"
+	testInstanceID      = "58f8c703-1756-48ab-9299-a847974d1fee"
+	testOperationID     = "fd5cee4d-0eeb-40d0-a7a7-0708e5eba470"
 	testSubAccountID    = "12df5747-3efb-4df6-ad6f-4414bb661ce3"
 	testGlobalAccountID = "80ac17bd-33e8-4ffa-8d56-1d5367755723"
 )
