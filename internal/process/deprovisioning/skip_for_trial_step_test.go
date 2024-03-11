ddf47daa26140ba483273ccc1f42c761594bcf29
ukff <110393214+ukff@users.noreply.github.com>
2024-03-11 12:24:39 +0100

    Clean not used managers

--- internal/process/deprovisioning/skip_for_trial_step_test.go
+++ internal/process/deprovisioning/skip_for_trial_step_test.go
@@ -1,8 +1,2 @@
 package deprovisioning
 
-const (
-	instanceID      = "58f8c703-1756-48ab-9299-a847974d1fee"
-	operationID     = "fd5cee4d-0eeb-40d0-a7a7-0708e5eba470"
-	subAccountID    = "12df5747-3efb-4df6-ad6f-4414bb661ce3"
-	globalAccountID = "80ac17bd-33e8-4ffa-8d56-1d5367755723"
-)
