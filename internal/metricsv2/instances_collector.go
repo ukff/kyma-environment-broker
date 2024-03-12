6709d106542d64de911c46ccdb18a76222cd07b9
ukff <110393214+ukff@users.noreply.github.com>
2024-03-12 18:46:58 +0100

    wip

--- internal/metricsv2/instances_collector.go
+++ internal/metricsv2/instances_collector.go
@@ -80,3 +80,23 @@ func (c *InstancesCollector) Collect(ch chan<- prometheus.Metric) {
 		collect(ch, c.licenseTypeDesc, num, t)
 	}
 }
+
+
+//
+// COPY OF THE internal/metrics/operations_collector.go for test porpuses, will be refactored
+//
+
+func collect(ch chan<- prometheus.Metric, desc *prometheus.Desc, value int, labelValues ...string) {
+	m, err := prometheus.NewConstMetric(
+		desc,
+		prometheus.GaugeValue,
+		float64(value),
+		labelValues...)
+	
+	if err != nil {
+		logrus.Errorf("unable to register metric %s", err.Error())
+		return
+	}
+	ch <- m
+}
+
