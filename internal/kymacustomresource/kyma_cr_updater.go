ac900f38cae8b8822b7c6766c92066074990f34a
Marcin Szwed <marcin.szwed@sap.com>
2024-03-13 11:23:47 +0100

    Add Kyma CR Updater (#529)
    * Add Kyma CR updater
    
    * Handle Kyma CR(s) updates when queued
    
    * Pass Kyma GVR as arg to constructor
    
    * Replace rest config with dynamic k8s client in Updater constructor
    
    * Fix logs
    
    * Add unit test
    
    * Add more unit tests

---
+++ internal/kymacustomresource/kyma_cr_updater.go
@@ -0,0 +1,86 @@
+package kymacustomresource
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/kyma-project/kyma-environment-broker/internal/syncqueues"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+)
+
+const (
+	namespace               = "kcp-system"
+	subaccountIdLabelKey    = "kyma-project.io/subaccount-id"
+	subaccountIdLabelFormat = "kyma-project.io/subaccount-id=%s"
+	betaEnabledLabelKey     = "kyma-project.io/beta-enabled"
+	emptyQueueSleepDuration = 30 * time.Second
+)
+
+type Updater struct {
+	k8sClient dynamic.Interface
+	queue     syncqueues.PriorityQueue
+	kymaGVR   schema.GroupVersionResource
+	logger    *slog.Logger
+}
+
+func NewUpdater(k8sClient dynamic.Interface, queue syncqueues.PriorityQueue, gvr schema.GroupVersionResource) (*Updater, error) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	return &Updater{
+		k8sClient: k8sClient,
+		queue:     queue,
+		kymaGVR:   gvr,
+		logger:    logger,
+	}, nil
+}
+
+func (u *Updater) Run() error {
+	for {
+		if u.queue.IsEmpty() {
+			time.Sleep(emptyQueueSleepDuration)
+			continue
+		}
+		item := u.queue.Extract()
+		unstructuredList, err := u.k8sClient.Resource(u.kymaGVR).Namespace(namespace).List(context.Background(), metav1.ListOptions{
+			LabelSelector: fmt.Sprintf(subaccountIdLabelFormat, item.SubaccountID),
+		})
+		if err != nil {
+			u.logger.Warn("while listing Kyma CRs: " + err.Error() + "adding item back to the queue")
+			u.queue.Insert(item)
+			continue
+		}
+		if len(unstructuredList.Items) == 0 {
+			u.logger.Info("no Kyma CRs found for subaccount" + item.SubaccountID)
+			continue
+		}
+		retryRequired := false
+		for _, kymaCrUnstructured := range unstructuredList.Items {
+			if err := u.updateBetaEnabledLabel(kymaCrUnstructured, item.BetaEnabled); err != nil {
+				u.logger.Warn("while updating Kyma CR: " + err.Error() + "item will be added back to the queue")
+				retryRequired = true
+			}
+		}
+		if retryRequired {
+			u.logger.Info("adding item back to the queue")
+			u.queue.Insert(item)
+		}
+	}
+}
+
+func (u *Updater) updateBetaEnabledLabel(un unstructured.Unstructured, betaEnabled string) error {
+	labels := un.GetLabels()
+	if labels == nil {
+		labels = make(map[string]string)
+	}
+	labels[betaEnabledLabelKey] = betaEnabled
+	un.SetLabels(labels)
+
+	_, err := u.k8sClient.Resource(u.kymaGVR).Namespace(namespace).Update(context.Background(), &un, metav1.UpdateOptions{})
+	return err
+}
---
+++ internal/kymacustomresource/kyma_cr_updater_test.go
@@ -0,0 +1,236 @@
+package kymacustomresource
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/kyma-project/kyma-environment-broker/internal/syncqueues"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic/fake"
+)
+
+// Kyma CR K8s data
+const (
+	group    = "operator.kyma-project.io"
+	version  = "v1beta2"
+	resource = "kymas"
+	kind     = "Kyma"
+)
+
+const (
+	subaccountID = "subaccount-id-1"
+	interval     = 100 * time.Millisecond
+	timeout      = 2 * time.Second
+)
+
+func TestUpdater(t *testing.T) {
+	// given
+	gvr := schema.GroupVersionResource{Group: group, Version: version, Resource: resource}
+	gvk := gvr.GroupVersion().WithKind(kind)
+	listGVK := gvk
+	listGVK.Kind += "List"
+
+	var kymaKind, kymaKindList unstructured.Unstructured
+	kymaKind.SetGroupVersionKind(gvk)
+	kymaKindList.SetGroupVersionKind(listGVK)
+
+	scheme := runtime.NewScheme()
+	scheme.AddKnownTypes(gvr.GroupVersion(), &kymaKind, &kymaKindList)
+
+	t.Run("should not update Kyma CRs when the queue is empty", func(t *testing.T) {
+		// given
+		kymaCRName := "kyma-cr-1"
+		mockKymaCR := &unstructured.Unstructured{}
+		mockKymaCR.SetGroupVersionKind(gvk)
+		mockKymaCR.SetName(kymaCRName)
+		mockKymaCR.SetNamespace(namespace)
+		require.NoError(t, unstructured.SetNestedField(mockKymaCR.Object, nil, "metadata", "creationTimestamp"))
+
+		var queue syncqueues.PriorityQueue
+		queue = &fakePriorityQueue{}
+		fakeK8sClient := fake.NewSimpleDynamicClient(scheme, mockKymaCR)
+		updater, err := NewUpdater(fakeK8sClient, queue, gvr)
+		require.NoError(t, err)
+
+		// when
+		go func(t *testing.T) {
+			require.NoError(t, updater.Run())
+		}(t)
+
+		// then
+		assert.True(t, queue.IsEmpty())
+
+		actual, err := fakeK8sClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), kymaCRName, metav1.GetOptions{})
+		require.NoError(t, err)
+		assert.NotContains(t, actual.GetLabels(), betaEnabledLabelKey)
+	})
+
+	t.Run("should update a Kyma CR with the given subaccount id label when the queue has a matching element", func(t *testing.T) {
+		// given
+		kymaCRName := "kyma-cr-1"
+		mockKymaCR := &unstructured.Unstructured{}
+		mockKymaCR.SetGroupVersionKind(gvk)
+		mockKymaCR.SetName(kymaCRName)
+		mockKymaCR.SetNamespace(namespace)
+		mockKymaCR.SetLabels(map[string]string{subaccountIdLabelKey: subaccountID})
+		require.NoError(t, unstructured.SetNestedField(mockKymaCR.Object, nil, "metadata", "creationTimestamp"))
+
+		var queue syncqueues.PriorityQueue
+		queue = &fakePriorityQueue{}
+		queue.Insert(syncqueues.QueueElement{
+			SubaccountID: subaccountID,
+			BetaEnabled:  "true",
+			ModifiedAt:   time.Now().Unix(),
+		})
+		assert.False(t, queue.IsEmpty())
+
+		fakeK8sClient := fake.NewSimpleDynamicClient(scheme, mockKymaCR)
+		updater, err := NewUpdater(fakeK8sClient, queue, gvr)
+		require.NoError(t, err)
+
+		// when
+		go func(t *testing.T) {
+			require.NoError(t, updater.Run())
+		}(t)
+
+		// then
+		err = wait.PollImmediate(interval, timeout, func() (bool, error) {
+			actual, err := fakeK8sClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), kymaCRName, metav1.GetOptions{})
+			require.NoError(t, err)
+			if actual.GetLabels()[betaEnabledLabelKey] == "true" {
+				return true, nil
+			}
+			return false, nil
+		})
+		require.NoError(t, err)
+		assert.True(t, queue.IsEmpty())
+	})
+
+	t.Run("should update all Kyma CRs with the given subaccount id label when the queue has a matching element", func(t *testing.T) {
+		// given
+		kymaCRName1, kymaCRName2 := "kyma-cr-1", "kyma-cr-2"
+		mockKymaCR1 := &unstructured.Unstructured{}
+		mockKymaCR1.SetGroupVersionKind(gvk)
+		mockKymaCR1.SetName(kymaCRName1)
+		mockKymaCR1.SetNamespace(namespace)
+		mockKymaCR1.SetLabels(map[string]string{subaccountIdLabelKey: subaccountID})
+		require.NoError(t, unstructured.SetNestedField(mockKymaCR1.Object, nil, "metadata", "creationTimestamp"))
+
+		mockKymaCR2 := mockKymaCR1.DeepCopy()
+		mockKymaCR2.SetName(kymaCRName2)
+
+		var queue syncqueues.PriorityQueue
+		queue = &fakePriorityQueue{}
+		queue.Insert(syncqueues.QueueElement{
+			SubaccountID: subaccountID,
+			BetaEnabled:  "true",
+			ModifiedAt:   time.Now().Unix(),
+		})
+		assert.False(t, queue.IsEmpty())
+
+		fakeK8sClient := fake.NewSimpleDynamicClient(scheme, mockKymaCR1, mockKymaCR2)
+		updater, err := NewUpdater(fakeK8sClient, queue, gvr)
+		require.NoError(t, err)
+
+		// when
+		go func(t *testing.T) {
+			require.NoError(t, updater.Run())
+		}(t)
+
+		// then
+		err = wait.PollImmediate(interval, timeout, func() (bool, error) {
+			actual, err := fakeK8sClient.Resource(gvr).Namespace(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: fmt.Sprintf(subaccountIdLabelFormat, subaccountID)})
+			assert.Len(t, actual.Items, 2)
+			require.NoError(t, err)
+			for _, un := range actual.Items {
+				if un.GetLabels()[betaEnabledLabelKey] != "true" {
+					return false, nil
+				}
+			}
+			return true, nil
+		})
+		require.NoError(t, err)
+		assert.True(t, queue.IsEmpty())
+	})
+
+	t.Run("should update just one Kyma CR with the given subaccount id label when the queue has a matching element", func(t *testing.T) {
+		// given
+		kymaCRName1, kymaCRName2 := "kyma-cr-1", "kyma-cr-2"
+		otherSubaccountID := "subaccount-id-2"
+
+		mockKymaCR1 := &unstructured.Unstructured{}
+		mockKymaCR1.SetGroupVersionKind(gvk)
+		mockKymaCR1.SetName(kymaCRName1)
+		mockKymaCR1.SetNamespace(namespace)
+		require.NoError(t, unstructured.SetNestedField(mockKymaCR1.Object, nil, "metadata", "creationTimestamp"))
+
+		mockKymaCR2 := mockKymaCR1.DeepCopy()
+		mockKymaCR2.SetName(kymaCRName2)
+
+		mockKymaCR1.SetLabels(map[string]string{subaccountIdLabelKey: subaccountID})
+		mockKymaCR2.SetLabels(map[string]string{subaccountIdLabelKey: otherSubaccountID})
+
+		var queue syncqueues.PriorityQueue
+		queue = &fakePriorityQueue{}
+		queue.Insert(syncqueues.QueueElement{
+			SubaccountID: subaccountID,
+			BetaEnabled:  "true",
+			ModifiedAt:   time.Now().Unix(),
+		})
+		assert.False(t, queue.IsEmpty())
+
+		fakeK8sClient := fake.NewSimpleDynamicClient(scheme, mockKymaCR1, mockKymaCR2)
+		updater, err := NewUpdater(fakeK8sClient, queue, gvr)
+		require.NoError(t, err)
+
+		// when
+		go func(t *testing.T) {
+			require.NoError(t, updater.Run())
+		}(t)
+
+		// then
+		err = wait.PollImmediate(interval, timeout, func() (bool, error) {
+			actual, err := fakeK8sClient.Resource(gvr).Namespace(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: fmt.Sprintf(subaccountIdLabelFormat, subaccountID)})
+			require.NoError(t, err)
+			assert.Len(t, actual.Items, 1)
+			for _, un := range actual.Items {
+				if un.GetLabels()[betaEnabledLabelKey] != "true" {
+					return false, nil
+				}
+			}
+			return true, nil
+		})
+		require.NoError(t, err)
+
+		actual, err := fakeK8sClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), kymaCRName2, metav1.GetOptions{})
+		require.NoError(t, err)
+		assert.NotContains(t, actual.GetLabels(), betaEnabledLabelKey)
+		assert.True(t, queue.IsEmpty())
+	})
+}
+
+type fakePriorityQueue struct {
+	elements []syncqueues.QueueElement
+}
+
+func (f *fakePriorityQueue) Insert(e syncqueues.QueueElement) {
+	f.elements = append(f.elements, e)
+}
+
+func (f *fakePriorityQueue) Extract() syncqueues.QueueElement {
+	extractedElement := f.elements[0]
+	f.elements = f.elements[1:]
+	return extractedElement
+}
+
+func (f *fakePriorityQueue) IsEmpty() bool {
+	return len(f.elements) == 0
+}
