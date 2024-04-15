a96f4c6021660f8bce007720b13add67ad78d348
ukff <110393214+ukff@users.noreply.github.com>
2024-04-15 12:26:42 +0200

    wip

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -76,7 +76,7 @@ import (
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
-
+// test
 // Config holds configuration for the whole application
 type Config struct {
 	// DbInMemory allows to use memory storage instead of the postgres one.
