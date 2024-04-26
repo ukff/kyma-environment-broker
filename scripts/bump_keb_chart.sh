1797c877612a555344fad8934a7aae570243fde8
ukff <110393214+ukff@users.noreply.github.com>
2024-04-26 15:53:35 +0200

    Revert "wip"
    This reverts commit 0d1fd5e5b385666f00548c91131c1d926995588e.

--- scripts/bump_keb_chart.sh
+++ scripts/bump_keb_chart.sh
@@ -17,7 +17,20 @@ VALUES_YAML="resources/keb/values.yaml"
 KEYS=$(yq e '.global.images | keys | .[]' $VALUES_YAML | grep 'kyma_environment')
 
 # bump images in resources/keb/values.yaml
+for key in $KEYS
+do
+    # yq removes empty lines when editing files, so the diff and patch are used to preserve formatting.
+    yq e ".global.images.$key.version = \"$RELEASE_TAG\"" $VALUES_YAML > $VALUES_YAML.new
+    yq '.' $VALUES_YAML > $VALUES_YAML.noblanks
+    diff -B $VALUES_YAML.noblanks $VALUES_YAML.new > resources/keb/patch.file
+    patch $VALUES_YAML resources/keb/patch.file 
+    rm $VALUES_YAML.noblanks
+    rm resources/keb/patch.file
+    rm $VALUES_YAML.new
+done
 
+yq e ".spec.jobTemplate.spec.template.spec.containers[0].image = \"europe-docker.pkg.dev/kyma-project/prod/kyma-environments-cleanup-job:$RELEASE_TAG\"" -i utils/kyma-environments-cleanup-job/kyma-environments-cleanup-job.yaml
+yq e ".spec.jobTemplate.spec.template.spec.containers[0].image = \"europe-docker.pkg.dev/kyma-project/prod/kyma-environment-archiver-job:$RELEASE_TAG\"" -i utils/archiver/kyma-environment-broker-archiver.yaml
 yq e ".version = \"$RELEASE_TAG\"" -i resources/keb/Chart.yaml
 yq e ".appVersion = \"$RELEASE_TAG\"" -i resources/keb/Chart.yaml
 
