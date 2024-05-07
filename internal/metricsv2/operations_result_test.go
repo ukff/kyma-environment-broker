ab6728837591773d167a881c5049ec62b2557490
ukff <110393214+ukff@users.noreply.github.com>
2024-05-07 10:30:52 +0200

    wip

--- internal/metricsv2/operations_result_test.go
+++ internal/metricsv2/operations_result_test.go
@@ -9,6 +9,8 @@ import (
 	
 	"github.com/google/uuid"
 	"github.com/kyma-project/kyma-environment-broker/internal"
+	`github.com/kyma-project/kyma-environment-broker/internal/event`
+	`github.com/kyma-project/kyma-environment-broker/internal/process`
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
 	"github.com/pivotal-cf/brokerapi/v8/domain"
 	"github.com/prometheus/client_golang/prometheus/testutil"
@@ -68,7 +70,7 @@ func TestOperationsResult(t *testing.T) {
 		// job seeking now in time windows
 
 		// simulate new op
-		newOp := getRandomOp(domain.InProgress)
+		newOp := getRandomOp(randomCreatedAt(), domain.InProgress)
 		newOp.CreatedAt = time.Now().UTC()
 		fmt.Println(fmt.Sprintf("newOp created: %s", newOp.CreatedAt))
 		err = operations.InsertOperation(newOp)
@@ -87,26 +89,37 @@ func TestOperationsResult(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, float64(1), testutil.ToFloat64(operationResult.metrics.With(getLabels(newOp))))
 		
-		// non existings ops
-		nonExistingOp1 := getRandomOp(domain.InProgress)
-		nonExistingOp2 := getRandomOp(domain.Failed)
+		newOpEvent := getRandomOp(randomCreatedAt(), domain.InProgress)
+		eventBroker := event.NewPubSub(logrus.New())
+		eventBroker.Subscribe(process.OperationFinished{}, operationResult.Handler)
+		eventBroker.Publish(context.Background(), process.OperationFinished{Operation: newOpEvent})
+		time.Sleep(10 * time.Second)
+		assert.Equal(t, float64(1), testutil.ToFloat64(operationResult.metrics.With(getLabels(newOpEvent))))
+		
+		
+		// non existings ops in DB
+		nonExistingOp1 := getRandomOp(randomCreatedAt(), domain.InProgress)
+		nonExistingOp2 := getRandomOp(randomCreatedAt(), domain.Failed)
 		time.Sleep(10 * time.Second)
 
 		// wait for job
 		assert.Equal(t, float64(0), testutil.ToFloat64(operationResult.metrics.With(getLabels(nonExistingOp1))))
 		assert.Equal(t, float64(0), testutil.ToFloat64(operationResult.metrics.With(getLabels(nonExistingOp2))))
 		
-		//new ops
-		existingOp1 := getRandomOp(domain.InProgress)
+		//new ops in DB
+		existingOp1 := getRandomOp(time.Now().UTC(), domain.InProgress)
 		operations.InsertOperation(existingOp1)
-		existingOp2 := getRandomOp(domain.Succeeded)
-		operations.InsertOperation(existingOp2)
-		existingOp3 := getRandomOp(domain.InProgress)
-		operations.InsertOperation(existingOp3)
-		existingOp4 := getRandomOp(domain.Failed)
-		operations.InsertOperation(existingOp4)
 		
-		time.Sleep(10 * time.Second)
+		existingOp2 := getRandomOp(time.Now().UTC(), domain.Succeeded)
+		operations.InsertOperation(existingOp2)
+		
+		existingOp3 := getRandomOp(time.Now().UTC(), domain.InProgress)
+		operations.InsertOperation(existingOp3)
+		
+		existingOp4 := getRandomOp(time.Now().UTC(),domain.Failed)
+ 		operations.InsertOperation(existingOp4)
+		
+		time.Sleep(20 * time.Second)
 		
 		// wait for job
 		assert.Equal(t, float64(1), testutil.ToFloat64(operationResult.metrics.With(getLabels(existingOp1))))
@@ -116,17 +129,17 @@ func TestOperationsResult(t *testing.T) {
 	})
 }
 
-func getRandomOp(op domain.LastOperationState) internal.Operation {
+func getRandomOp(createdAt time.Time, state domain.LastOperationState) internal.Operation {
 	return internal.Operation{
 		ID:         uuid.New().String(),
 		InstanceID: uuid.New().String(),
 		ProvisioningParameters: internal.ProvisioningParameters{
 			PlanID: randomPlanId(),
 		},
-		CreatedAt: randomCreatedAt(),
+		CreatedAt: createdAt,
 		UpdatedAt: randomUpdatedAtAfterCreatedAt(),
 		Type:      randomType(),
-		State:     op,
+		State:     state,
 	}
 }
 
