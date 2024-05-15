c1936b1870c2249ded796aa1a2b9251c8bea1313
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2024-05-15 11:25:57 +0200

    EDP Log Levels Corrections (#764)
    * EDP Log Leves Corrections
    
    * Corrected Error Placement
    
    * Corrected Error Level
    
    * Linter

--- internal/process/deprovisioning/deregister_cluster.go
+++ internal/process/deprovisioning/deregister_cluster.go
@@ -52,12 +52,10 @@ func (s *DeregisterClusterStep) Run(operation internal.Operation, log logrus.Fie
 }
 
 func (s *DeregisterClusterStep) handleError(operation internal.Operation, err error, log logrus.FieldLogger, msg string) (internal.Operation, time.Duration, error) {
-	log.Errorf("%s: %s", msg, err)
-
 	if kebErrors.IsTemporaryError(err) {
 		since := time.Since(operation.UpdatedAt)
 		if since < 30*time.Minute {
-			log.Errorf("request to the Reconciler failed: %s. Retry...", err)
+			log.Warnf("request to the Reconciler failed: %s. Retry...", err)
 			return operation, 15 * time.Second, nil
 		}
 	}
@@ -66,5 +64,8 @@ func (s *DeregisterClusterStep) handleError(operation internal.Operation, err er
 	if repeat != 0 {
 		return operation, repeat, err
 	}
+
+	log.Errorf("%s: %s", msg, err)
+
 	return operation, 0, nil
 }
--- internal/process/deprovisioning/edp_deregistration.go
+++ internal/process/deprovisioning/edp_deregistration.go
@@ -97,12 +97,10 @@ func (s *EDPDeregistrationStep) Run(operation internal.Operation, log logrus.Fie
 }
 
 func (s *EDPDeregistrationStep) handleError(operation internal.Operation, err error, log logrus.FieldLogger, msg string) (internal.Operation, time.Duration, error) {
-	log.Errorf("%s: %s", msg, err)
-
 	if kebError.IsTemporaryError(err) {
 		since := time.Since(operation.UpdatedAt)
 		if since < time.Minute*30 {
-			log.Errorf("request to EDP failed: %s. Retry...", err)
+			log.Warnf("request to EDP failed: %s. Retry...", err)
 			return operation, 10 * time.Second, nil
 		}
 	}
@@ -112,5 +110,8 @@ func (s *EDPDeregistrationStep) handleError(operation internal.Operation, err er
 	if repeat != 0 {
 		return operation, repeat, err
 	}
+
+	log.Errorf("%s: %s", msg, err)
+
 	return operation, 0, nil
 }
