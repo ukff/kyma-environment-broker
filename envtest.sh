68600372fdc27d5e337f4b75656dd5f073fce350
ukff <110393214+ukff@users.noreply.github.com>
2024-09-19 14:34:41 +0200

    wip

--- envtest.sh
+++ envtest.sh
@@ -8,7 +8,7 @@ K8S_VERSION=1.29.1
 
 #check if setup-envtest is installed or check if currently installed setup-envtest contains assets for requested k8s version
 if [ ! -e "$LOCAL_BIN/setup-envtest" ] || [ -z $("$LOCAL_BIN"/setup-envtest list -i  | awk '{print $2}' | tr -d v | grep "$K8S_VERSION") ]; then
-  GOBIN="$LOCAL_BIN" go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+  GOBIN="$LOCAL_BIN" go install sigs.k8s.io/controller-runtime/tools/setup-envtest@release-0.17
   if [ $? -ne 0 ]; then
     echo "Error: failed to install setup-envtest"
     exit $?
