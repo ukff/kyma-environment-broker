5d6566e70a5c6222f8566ee02002e14fe0d86a2e
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2024-09-20 10:27:17 +0200

    `setup-envtest`  workaround (#1163)
    * env test test
    
    * trace shell script
    
    * print only PID
    
    * hack again
    
    * no print
    
    * standard error handling
    
    * almost standard error handling
    
    * almost

--- envtest.sh
+++ envtest.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
+
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+
 cd "$(dirname "$0")" || exit
 
-LOCAL_BIN=$(pwd)/bin
+LOCAL_BIN=$(pwd)/bin/$$
 mkdir -p "$LOCAL_BIN"
 
 K8S_VERSION=1.29.1
 
-#check if setup-envtest is installed or check if currently installed setup-envtest contains assets for requested k8s version
-if [ ! -e "$LOCAL_BIN/setup-envtest" ] || [ -z $("$LOCAL_BIN"/setup-envtest list -i  | awk '{print $2}' | tr -d v | grep "$K8S_VERSION") ]; then
-  GOBIN="$LOCAL_BIN" go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
-  if [ $? -ne 0 ]; then
-    echo "Error: failed to install setup-envtest"
-    exit $?
-  fi
-fi
+GOBIN="$LOCAL_BIN" go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 
 output=$("$LOCAL_BIN"/setup-envtest use --bin-dir "$LOCAL_BIN" -p path "$K8S_VERSION")
-if [ $? -ne 0 ]; then
-  echo "Error: failed to run setup-envtest"
-  exit $?
-fi
 echo "$output"
