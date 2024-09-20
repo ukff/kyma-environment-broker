21a8aa91929a845db9dde84b842e60a2a479baf7
ukff <110393214+ukff@users.noreply.github.com>
2024-09-20 15:29:59 +0200

    wip

--- internal/k8s/metadata.go
+++ internal/k8s/metadata.go
@@ -13,32 +13,7 @@ const (
 	Labels      MetadataType = 1
 )
 
-func OverrideMetadata(k8sObject *unstructured.Unstructured, metadataType MetadataType, values map[string]string) error {
-	if k8sObject == nil {
-		return fmt.Errorf("object is nil")
-	}
-
-	if metadataType != Labels && metadataType != Annotations {
-		return fmt.Errorf("unknown metadata type")
-	}
-
-	switch metadataType {
-	case Labels:
-		{
-			(*k8sObject).SetLabels(values)
-		}
-	case Annotations:
-		{
-			(*k8sObject).SetAnnotations(values)
-		}
-	default:
-		return fmt.Errorf("unknown metadata type")
-	}
-
-	return nil
-}
-
-func ChangeSingleMetadata(k8sObject *unstructured.Unstructured, metadataType MetadataType, key, value string) error {
+func ChangeMetadata(k8sObject *unstructured.Unstructured, metadataType MetadataType, key, value string) error {
 	if k8sObject == nil {
 		return fmt.Errorf("object is nil")
 	}
@@ -58,39 +33,6 @@ func ChangeSingleMetadata(k8sObject *unstructured.Unstructured, metadataType Met
 		{
 			annotations := (*k8sObject).GetAnnotations()
 			annotations[key] = value
-			(*k8sObject).SetAnnotations(annotations)
-		}
-	default:
-		return fmt.Errorf("unknown metadata type")
-	}
-
-	return nil
-}
-
-func ChangeManyMetadata(k8sObject *unstructured.Unstructured, metadataType MetadataType, values map[string]string) error {
-	if k8sObject == nil {
-		return fmt.Errorf("object is nil")
-	}
-
-	if metadataType != Labels && metadataType != Annotations {
-		return fmt.Errorf("unknown metadata type")
-	}
-
-	switch metadataType {
-	case Labels:
-		{
-			labels := (*k8sObject).GetLabels()
-			for key, value := range values {
-				labels[key] = value
-			}
-			(*k8sObject).SetLabels(labels)
-		}
-	case Annotations:
-		{
-			annotations := (*k8sObject).GetAnnotations()
-			for key, value := range values {
-				annotations[key] = value
-			}
 			(*k8sObject).SetAnnotations(annotations)
 		}
 	default:
