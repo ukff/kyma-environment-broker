e5adb04339842df3f13b71eddbfc7f071ec3e66c
ukff <110393214+ukff@users.noreply.github.com>
2024-09-23 09:22:57 +0200

    wip

---
+++ internal/k8s/kyma_labels.go
@@ -0,0 +1,5 @@
+package k8s
+
+const (
+	GlobalAccountIdLabel = "kyma-project.io/global-account-id"
+)
--- internal/k8s/metadata.go
+++ internal/k8s/metadata.go
@@ -6,43 +6,21 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-type MetadataType int
-
-const (
-	Annotations MetadataType = 0
-	Labels      MetadataType = 1
-)
-
 const (
 	GlobalAccountIdLabel = "kyma-project.io/global-account-id"
 )
 
-func AddOrOverrideMetadata(k8sObject *unstructured.Unstructured, metadataType MetadataType, key, value string) error {
+func AddOrOverrideMetadata(k8sObject *unstructured.Unstructured, key, value string) error {
 	if k8sObject == nil {
 		return fmt.Errorf("object is nil")
 	}
 
-	if metadataType != Labels && metadataType != Annotations {
-		return fmt.Errorf("unknown metadata type")
+	labels := (*k8sObject).GetLabels()
+	if labels == nil {
+		labels = make(map[string]string)
 	}
-
-	switch metadataType {
-	case Labels:
-		{
-			labels := (*k8sObject).GetLabels()
-			if labels == nil {
-				labels = make(map[string]string)
-			}
-			labels[key] = value
-			(*k8sObject).SetLabels(labels)
-		}
-	case Annotations:
-		{
-			//TBA
-		}
-	default:
-		return fmt.Errorf("unknown metadata type")
-	}
+	labels[key] = value
+	(*k8sObject).SetLabels(labels)
 
 	return nil
 }
