9c595d49d2fc136b7199e50db502c05bed866eda
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2024-09-30 16:08:57 +0200

    Disable tracing for label validator (#1221)

--- scripts/label_validator.sh
+++ scripts/label_validator.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
