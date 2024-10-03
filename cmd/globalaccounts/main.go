a2ef6ab19dc0851155bdf3bb58caa06ddeff505d
ukff <110393214+ukff@users.noreply.github.com>
2024-10-03 11:35:57 +0200

    wip

--- cmd/globalaccounts/main.go
+++ cmd/globalaccounts/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/kyma-project/kyma-environment-broker/internal/globalaccounts"
+	"github.com/vrischmann/envconfig"
 )
 
 func main() {
-	globalaccounts.Run(globalaccounts.Config{})
+	var cfg globalaccounts.Config
+	err := envconfig.InitWithPrefix(&cfg, "GLOBALACCOUNTS")
+	if err != nil {
+		panic(err.Error())
+	}
+	globalaccounts.Run(cfg)
 }
