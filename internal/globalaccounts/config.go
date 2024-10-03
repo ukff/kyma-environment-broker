6d112552d5efe515f7ac56b91fb51ce17783ba71
ukff <110393214+ukff@users.noreply.github.com>
2024-10-03 13:25:39 +0200

    wip

--- internal/globalaccounts/config.go
+++ internal/globalaccounts/config.go
@@ -1,13 +1,11 @@
 package globalaccounts
 
 import (
-	"github.com/kyma-project/kyma-environment-broker/internal/broker"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
 )
 
 type Config struct {
 	Database             storage.Config
-	Broker               broker.ClientConfig
 	DryRun               bool   `envconfig:"default=true"`
 	AccountServiceID     string `envconfig:"default=account-service-id"`
 	AccountServiceSecret string `envconfig:"default=account-service-secret"`
