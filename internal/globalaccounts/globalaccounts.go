54ca8f24aced8e821edd34355a798535f51bf237
ukff <110393214+ukff@users.noreply.github.com>
2024-10-03 13:27:25 +0200

    wip

--- internal/globalaccounts/globalaccounts.go
+++ internal/globalaccounts/globalaccounts.go
@@ -118,7 +118,7 @@ func getKcpClient() (client.Client, error) {
 
 func clusterOp(ctx context.Context, kcp client.Client, logs *logrus.Logger) (unstructured.UnstructuredList, error) {
 	gvk, err := k8s.GvkByName(k8s.KymaCr)
-	if condition := err != nil; condition {
+	if err != nil {
 		logs.Errorf("error getting GVK %s", err)
 		return unstructured.UnstructuredList{}, nil
 	}
