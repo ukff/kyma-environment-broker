924451766b38c5efeefcc5fbb94147b400f46d4d
KsaweryZietara <91937141+KsaweryZietara@users.noreply.github.com>
2024-10-28 09:07:03 +0100

    Create e2e provisioning API test (#1371)
    * Create e2e provisioning API test
    
    * Test bindings API
    
    * Test secret existence with kubeconfig from binding
    
    * Refactor files structure
    
    * Rename folder
    
    * Update docs
    
    * Rename test
    
    * Change require no error to assertion
    
    * Update end-2-end tests docs
    
    * Extract paths to functions
    
    * Update docs
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs/contributor/05-10-e2e_tests.md
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    * Update docs
    
    * Update docs
    
    * Update docs
    
    ---------
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>

---
+++ testing/e2e/skr/provisioning-service-test/internal/model.go
@@ -0,0 +1,65 @@
+package internal
+
+type AccessToken struct {
+	Token string `json:"access_token"`
+}
+
+type ErrorResponse struct {
+	Error Error `json:"error"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+type CreateEnvironmentRequest struct {
+	EnvironmentType string                `json:"environmentType"`
+	ServiceName     string                `json:"serviceName"`
+	PlanName        string                `json:"planName"`
+	User            string                `json:"user"`
+	Parameters      EnvironmentParameters `json:"parameters"`
+}
+
+type EnvironmentParameters struct {
+	Name   string `json:"name"`
+	Region string `json:"region"`
+}
+
+type CreatedEnvironmentResponse struct {
+	ID string `json:"id"`
+}
+
+type State string
+
+const (
+	CREATING        State = "CREATING"
+	UPDATING        State = "UPDATING"
+	DELETING        State = "DELETING"
+	OK              State = "OK"
+	CREATION_FAILED State = "CREATION_FAILED"
+	DELETION_FAILED State = "DELETION_FAILED"
+	UPDATE_FAILED   State = "UPDATE_FAILED"
+)
+
+type EnvironmentResponse struct {
+	ID    string `json:"id"`
+	State State  `json:"state"`
+}
+
+type CreateBindingRequest struct {
+	ServiceInstanceID string `json:"serviceInstanceId"`
+	PlanID            string `json:"planId"`
+}
+
+type CreatedBindingResponse struct {
+	ID          string
+	Credentials Credentials `json:"credentials"`
+}
+
+type GetBindingResponse struct {
+	Credentials Credentials `json:"credentials"`
+}
+
+type Credentials struct {
+	Kubeconfig string `json:"kubeconfig"`
+}
