4e97427d7ba050f9bd2666df70b1c2e7269d0f7b
ukff <110393214+ukff@users.noreply.github.com>
2024-11-27 08:33:47 +0100

    wip

--- internal/process/provisioning/start_step.go
+++ internal/process/provisioning/start_step.go
@@ -1,6 +1,8 @@
 package provisioning
 
 import (
+	"fmt"
+	kebError "github.com/kyma-project/kyma-environment-broker/internal/error"
 	"time"
 
 	"github.com/kyma-project/kyma-environment-broker/common/orchestration"
@@ -21,11 +23,11 @@ type StartStep struct {
 }
 
 func NewStartStep(os storage.Operations, is storage.Instances) *StartStep {
-	return &StartStep{
+	step := &StartStep{
 		operationStorage: os,
 		instanceStorage:  is,
-		operationManager: process.NewOperationManager(os),
 	}
+	step.operationManager = process.NewOperationManagerExtendent(os, step.Name(), kebError.ProvisionerDependency)
 }
 
 func (s *StartStep) Name() string {
@@ -33,6 +35,7 @@ func (s *StartStep) Name() string {
 }
 
 func (s *StartStep) Run(operation internal.Operation, log logrus.FieldLogger) (internal.Operation, time.Duration, error) {
+	return s.operationManager.OperationFailed(operation, "Starting step must be implemented", fmt.Errorf("technical error"), log)
 	if operation.State != orchestration.Pending {
 		return operation, 0, nil
 	}
