4f37d25834fbd7c49227db2fbf64a33517e78ee2
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2024-12-02 10:50:01 +0100

    Increase provisioning and deprovisioning duration metric range (#1513)

--- internal/metricsv2/operation_duration.go
+++ internal/metricsv2/operation_duration.go
@@ -32,14 +32,14 @@ func NewOperationDurationCollector(logger logrus.FieldLogger) *OperationDuration
 			Subsystem: prometheusSubsystemv2,
 			Name:      "provisioning_duration_minutes",
 			Help:      "The time of the provisioning process",
-			Buckets:   prometheus.LinearBuckets(10, 2, 56),
+			Buckets:   prometheus.LinearBuckets(6, 2, 58),
 		}, []string{"plan_id"}),
 		deprovisioningHistogram: prometheus.NewHistogramVec(prometheus.HistogramOpts{
 			Namespace: prometheusNamespacev2,
 			Subsystem: prometheusSubsystemv2,
 			Name:      "deprovisioning_duration_minutes",
 			Help:      "The time of the deprovisioning process",
-			Buckets:   prometheus.LinearBuckets(10, 2, 56),
+			Buckets:   prometheus.LinearBuckets(6, 2, 58),
 		}, []string{"plan_id"}),
 		logger: logger,
 	}
