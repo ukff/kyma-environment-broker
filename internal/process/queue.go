f196e69585197a5ba712d4f5e610f954add947b5
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2024-12-06 12:03:42 +0100

    Replace deprecated NewNamedRateLimitingQueue (#1543)
    Replace deorecated NewNamedRateLimitingQueue

--- internal/process/queue.go
+++ internal/process/queue.go
@@ -32,7 +32,7 @@ type Queue struct {
 func NewQueue(executor Executor, log logrus.FieldLogger, name string, warnAfterTime, healthCheckIntervalTime time.Duration) *Queue {
 	// add queue name field that could be logged later on
 	return &Queue{
-		queue:                   workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "operations"),
+		queue:                   workqueue.NewRateLimitingQueueWithConfig(workqueue.DefaultControllerRateLimiter(), workqueue.RateLimitingQueueConfig{Name: "operations"}),
 		executor:                executor,
 		waitGroup:               sync.WaitGroup{},
 		log:                     log.WithField("queueName", name),
