46a208920acd518e5700a02c08a115c7e491a40f
ukff <110393214+ukff@users.noreply.github.com>
2024-12-09 20:16:10 +0100

    wip

--- cmd/broker/provisioning.go
+++ cmd/broker/provisioning.go
@@ -113,9 +113,7 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *proce
 		},
 		{
 			stage:     createRuntimeStageName,
-			disabled:  cfg.InfrastructureManagerIntegrationDisabled,
 			step:      steps.NewCheckGardenerCluster(db.Operations(), cli, cfg.Broker.KimConfig, cfg.Provisioner.GardenerClusterStepTimeout),
-			condition: provisioning.SkipForOwnClusterPlan,
 		},
 		{ // TODO: this step must be removed when kubeconfig is created by IM only
 			stage: createRuntimeStageName,
