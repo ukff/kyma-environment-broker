63341326c85d45ff21b4452a910c7bb029100fa0
ukff <110393214+ukff@users.noreply.github.com>
2024-12-09 19:48:50 +0100

    wip

--- internal/process/steps/gardener_cluster.go
+++ internal/process/steps/gardener_cluster.go
@@ -62,7 +62,7 @@ func (s *checkGardenerCluster) Run(operation internal.Operation, log logrus.Fiel
 	gc, err := s.GetGardenerCluster(operation.RuntimeID, operation.KymaResourceNamespace)
 	// simulate error
 	if err == nil {
-		log.Errorf("unable to get GardenerCluster %s/%s", operation.KymaResourceNamespace, operation.RuntimeID)
+		log.Error("unable to get GardenerCluster %s/%s", operation.KymaResourceNamespace, operation.RuntimeID)
 		return s.operationManager.RetryOperation(operation, "unable to get GardenerCluster", err, time.Second, 10*time.Second, log)
 	}
 
